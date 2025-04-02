b7216ed9f3fa39f9db86f05e8d1345e30d355129
Igor Chapy <misha@mate.acedemy>
2025-04-02 22:25:08 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from .views import (
     index,
     CarListView,
-@@ -9,10 +8,14 @@
     CarDeleteView,
+    CarDetailView,
+    CarCreateView,
+    CarUpdateView,
     DriverListView,
     DriverDetailView,
     DriverCreateView,
@@ -17,88 +19,29 @@ from .views import (
     toggle_assign_to_car,
 )
 
+app_name = 'taxi'
+
 urlpatterns = [
     path("", index, name="index"),
-    path(
-        "manufacturers/",
-        ManufacturerListView.as_view(),
-        name="manufacturer-list",
-    ),
-    path(
-        "manufacturers/create/",
-        ManufacturerCreateView.as_view(),
-        name="manufacturer-create",
-    ),
-    path(
-        "manufacturers/<int:pk>/update/",
-        ManufacturerUpdateView.as_view(),
-        name="manufacturer-update",
-    ),
-    path(
-        "manufacturers/<int:pk>/delete/",
-        ManufacturerDeleteView.as_view(),
-        name="manufacturer-delete",
-    ),
+
+    # Manufacturers
+    path("manufacturers/", ManufacturerListView.as_view(), name="manufacturer-list"),
+    path("manufacturers/create/", ManufacturerCreateView.as_view(), name="manufacturer-create"),
+    path("manufacturers/<int:pk>/update/", ManufacturerUpdateView.as_view(), name="manufacturer-update"),
+    path("manufacturers/<int:pk>/delete/", ManufacturerDeleteView.as_view(), name="manufacturer-delete"),
+
+    # Cars
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
+    path("cars/<int:pk>/toggle-assign/", toggle_assign_to_car, name="toggle-assign-to-car"),
+
+    # Drivers
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
-        "cars/",
-        CarListView.as_view(),
-        name="car-list"
-    ),
-    path(
-        "cars/<int:pk>/",
-        CarDetailView.as_view(),
-        name="car-detail"
-    ),
-    path(
-        "cars/create/",
-        CarCreateView.as_view(),
-        name="car-create"
-    ),
-    path(
-        "cars/<int:pk>/update/",
-        CarUpdateView.as_view(),
-        name="car-update"
-    ),
-    path(
-        "cars/<int:pk>/delete/",
-        CarDeleteView.as_view(),
-        name="car-delete"
-    ),
-    path(
-        "drivers/",
-        DriverListView.as_view(),
-        name="driver-list"
-    ),
-    path(
-        "drivers/<int:pk>/",
-        DriverDetailView.as_view(),
-        name="driver-detail"
-    ),
-    path(
-        "drivers/create/",
-        DriverCreateView.as_view(),
-        name="driver-create"
-    ),
-    path(
-        "drivers/<int:pk>/update/",
-        DriverLicenseUpdateView.as_view(),
-        name="driver-update"
-    ),
-    path(
-        "drivers/<int:pk>/delete/",
-        DriverDeleteView.as_view(),
-        name="driver-delete"
-    ),
-    path(
-        "cars/<int:pk>/toggle-assign/",
-        toggle_assign_to_car,
-        name="toggle-assign-to-car"
-    ),
+    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
+    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
+    path("drivers/<int:pk>/update/", DriverLicenseUpdateView.as_view(), name="driver-update"),
+    path("drivers/<int:pk>/delete/", DriverDeleteView.as_view(), name="driver-delete"),
 ]
